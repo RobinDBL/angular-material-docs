6f606daa8f168522be18c128b03cb1bc4d444da3
Robin Deblauwe <robindeblauwe@telenet.be>
2022-08-22 13:33:51 +0200

    updated url generation

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -6,7 +6,7 @@ export class HttpService {
 
 constructor(private axios = http.default) { }
 
-public async getComponents() {
+public async getComponents(): Promise<Component[]> {
     // Fetch the angular-material component lists from their github page
 	let res = await this.axios.get("https://github.com/angular/components/tree/main/src/material");
 	// Get the html code
@@ -19,12 +19,27 @@ public async getComponents() {
 		dom.window.document.getElementsByClassName('js-navigation-open Link--primary'),
 		  );
     
+    // Create a list with component objects
+    let components: Component[] = [];
+    
     elements.forEach(element => {
         let component: Component = {
             name: element.innerHTML,
-            url: element.getAttribute("href"),
+            url: "https://material.angular.io/components/" + element.innerHTML + "/overview",
+            githubUrl: "https://raw.githubusercontent.",
+        };
+
+        if (!component.name.includes("legacy")){
+            if(!component.name.includes(".")) {
+                components.push(component);
+            }
         }
+
     });
+
+    return components;
+
+    // remove unwanted components such as legacy and files
 }
 
 }
