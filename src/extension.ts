0b359b1e9516a92f21267a74359fc6078abc83ff
Robin Deblauwe <robindeblauwe@telenet.be>
2022-08-22 13:34:42 +0200

    removed unused imports

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-import { QuickPickItem } from './models/QuickPickItem';
 import { HttpService } from './services/http.service';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
